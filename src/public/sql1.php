283443866ba094fdeedea566801417fa89bfe54d
YuDemura <Yu.demura1214@gmail.com>
2022-03-08 20:24:04 +0900

    データの取得ー課題

---
+++ src/public/sql1.php
@@ -0,0 +1,108 @@
+<?php
+
+$dbUserName = "root";
+
+$dbPassword = "password";
+
+$pdo = new PDO("mysql:host=mysql; dbname=tq_quest; charset=utf8", $dbUserName, $dbPassword);
+
+
+
+
+//incomesテーブルデータ
+
+$sql = "select * from incomes";
+
+$statement = $pdo->prepare($sql);
+
+$statement->execute();
+
+$result1 = $statement->fetchAll(PDO::FETCH_ASSOC);
+
+var_dump($result1);
+
+
+
+
+//spendingsテーブルデータ
+
+$sql = "select * from spendings";
+
+$statement = $pdo->prepare($sql);
+
+$statement->execute();
+
+$result2 = $statement->fetchAll(PDO::FETCH_ASSOC);
+
+var_dump($result2);
+
+
+
+
+//categoriesテーブルデータ
+
+$sql = "select * from categories";
+
+$statement = $pdo->prepare($sql);
+
+$statement->execute();
+
+$result3 = $statement->fetchAll(PDO::FETCH_ASSOC);
+
+var_dump($result3);
+
+
+
+
+<?php
+
+$dbUserName = "root";
+
+$dbPassword = "password";
+
+$pdo = new PDO("mysql:host=mysql; dbname=tq_quest; charset=utf8", $dbUserName, $dbPassword);
+
+
+
+
+//incomesテーブルデータ
+
+$sql = "select * from incomes";
+
+$statement = $pdo->prepare($sql);
+
+$statement->execute();
+
+$result1 = $statement->fetchAll(PDO::FETCH_ASSOC);
+
+var_dump($result1);
+
+
+
+
+//spendingsテーブルデータ
+
+$sql = "select * from spendings";
+
+$statement = $pdo->prepare($sql);
+
+$statement->execute();
+
+$result2 = $statement->fetchAll(PDO::FETCH_ASSOC);
+
+var_dump($result2);
+
+
+
+
+//categoriesテーブルデータ
+
+$sql = "select * from categories";
+
+$statement = $pdo->prepare($sql);
+
+$statement->execute();
+
+$result3 = $statement->fetchAll(PDO::FETCH_ASSOC);
+
+var_dump($result3);
